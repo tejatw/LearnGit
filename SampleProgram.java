08d73ed90961834ebac072586eef6d5ab2c1910e
tejatw <tejatw@gmail.com>
2016-09-07 09:21:45 +0530

    Update SampleProgram.java

--- SampleProgram.java
+++ SampleProgram.java
@@ -2,6 +2,7 @@ public class SampleProgram{
 
     public static void main(String args[]){
 
+        System.out.println("Master Branch");
         System.out.println("Development Branch");
     }
 }
